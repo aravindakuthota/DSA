e69e233ad7449b141acf64f6018c859bafd6927a
Aravind Akuthota <aravindakuthota543@gmail.com>
2025-07-10 11:35:22 +0530

    dsa level2

---
+++ Level2/selection_sort.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class Main{
+    static void selection_sort(int arr[] , int n ){
+        for(int i =0; i<n-1;i++){
+            int mini = i;
+            for(int j =i;j<n ;j++){
+                if(arr[j]<arr[mini]){
+                    mini = j;
+                }
+            }
+            int temp = arr[mini];
+            arr[mini] = arr[i];
+            arr[i] = temp;
+        }
+        System.out.println("After selection sort : ");
+        for(int i =0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+
+public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+
+
+        int[] arr = new int[n];
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+    selection_sort(arr,n);
+}
+}
